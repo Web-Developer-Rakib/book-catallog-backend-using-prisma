144f80349943b184728d3f830b0abf9bf68de2b0
Web-Developer-Rakib <rakibodesk7@gmail.com>
2023-09-03 12:26:12 +0600

    Delete user api created

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -58,3 +58,95 @@ export const getSingleUser = async (
     });
   }
 };
+export const updateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const { name, email, contactNo, address, profileImg } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        statusCode: 404,
+        message: "User not found.",
+      });
+    } else {
+      const updatedUser = await prisma.user.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+          email,
+          contactNo,
+          address,
+          profileImg,
+        },
+      });
+      res.status(200).json({
+        success: true,
+        statusCode: 200,
+        message: "User updated successfully",
+        data: updatedUser,
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: "Failed to update the user.",
+      error: error.message,
+    });
+  }
+};
+export const deleteUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        statusCode: 404,
+        message: "User not found.",
+      });
+    } else {
+      await prisma.user.delete({
+        where: {
+          id,
+        },
+      });
+      res.status(200).json({
+        success: true,
+        statusCode: 200,
+        message: "User deleted successfully",
+        data: user,
+      });
+    }
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: "Failed to delete the user.",
+      error: error.message,
+    });
+  }
+};
--- src/user/user.routes.ts
+++ src/user/user.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { getAllUsers, getSingleUser } from "./user.controller";
+import { getAllUsers, getSingleUser, updateUser } from "./user.controller";
 const userRouter = express.Router();
 
 userRouter.get("/", getAllUsers);
 userRouter.get("/:id", getSingleUser);
+userRouter.patch("/:id", updateUser);
+userRouter.delete("/:id", updateUser);
 
 export default userRouter;
