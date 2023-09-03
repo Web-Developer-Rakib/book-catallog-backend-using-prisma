24deb9f40c6e4978356b74052b4802b3b0538571
Web-Developer-Rakib <rakibodesk7@gmail.com>
2023-09-03 14:47:11 +0600

    Create category api created

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -7,7 +7,29 @@ export const createCategory = async (
   req: Request,
   res: Response,
   next: NextFunction
-): Promise<void> => {};
+): Promise<void> => {
+  try {
+    const { title } = req.body;
+    const category = await prisma.category.create({
+      data: {
+        title,
+      },
+    });
+    res.status(201).json({
+      success: true,
+      statusCode: 201,
+      message: "Category created successfully",
+      data: category,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: "Failed to create the category.",
+      error: error.message,
+    });
+  }
+};
 export const getAllCategories = async (
   req: Request,
   res: Response,
