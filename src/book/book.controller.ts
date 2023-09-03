b31ee7cd8de80ea278221656e10334bf3ac20057
Web-Developer-Rakib <rakibodesk7@gmail.com>
2023-09-03 15:10:19 +0600

    Get all books api created

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -44,7 +44,42 @@ export const getAllBooks = async (
   req: Request,
   res: Response,
   next: NextFunction
-): Promise<void> => {};
+): Promise<void> => {
+  try {
+    const { page = 1, size = 10 } = req.query;
+    const parsedPage = parseInt(page as string, 10);
+    const parsedSize = parseInt(size as string, 10);
+    const skip = (parsedPage - 1) * parsedSize;
+    const total = await prisma.book.count();
+    const books = await prisma.book.findMany({
+      skip,
+      take: parsedSize,
+      include: {
+        category: true,
+      },
+    });
+    const totalPage = Math.ceil(total / parsedSize);
+    res.status(200).json({
+      success: true,
+      statusCode: 200,
+      message: "Books fetched successfully",
+      meta: {
+        page: parsedPage,
+        size: parsedSize,
+        total,
+        totalPage,
+      },
+      data: books,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: "Failed to fetch books.",
+      error: error.message,
+    });
+  }
+};
 export const getBooksByCategory = async (
   req: Request,
   res: Response,
